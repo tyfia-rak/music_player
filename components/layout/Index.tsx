e27c9f25d8b28fabb179a583bb9e30fbe05ce8aa
unknown <sarobidy.rakotonarivo.1@gmail.com>
2025-03-17 10:05:33 +0300

    feat: creation and implementation of pages

--- components/layout/Index.tsx
+++ components/layout/Index.tsx
@@ -1,11 +1,20 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import MusicList from './MusicList';
+import Playing from './Playing';
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Index = () => {
+  const [tabSelected,setTabSelected] = useState<"list" | "playing">("playing");
   return (
-    <View className="flex h-screen items-center justify-center">
-      <Text className='text-3xl font-semibold'>je suis </Text>
-    </View>
+    <>
+    <LinearGradient colors={["#212528","#111315"]}>
+      <SafeAreaView>
+        {tabSelected === "list" ? <MusicList/> : <Playing/>}
+      </SafeAreaView>
+    </LinearGradient>
+    </>
   )
 }
 
---
+++ components/layout/Playing.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Playing = () => {
+  return (
+    <View className='flex h-screen items-center justify-center'>
+      <Text className='text-3xl font-semibold'>this is music playing</Text>
+    </View>
+  )
+}
+
+export default Playing
